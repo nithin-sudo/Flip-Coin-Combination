6e9a9a635f8e85e657a8fa5ff5264dc17feeef39
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-05-27 00:46:20 +0530

    singlet

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -1,9 +1,22 @@
 #!/bin/bash -x
 echo "Flip Coin Combination Problem"
 ran=$((RANDOM%2))
-if [ $ran -eq 1 ]
-then
-        echo "Coin faces HEAD"
-else
-        echo "Coin faces TAIL"
-fi
+echo "Flip Coin Combination Problem"
+declare -A dict
+read -p "Enter the number to flip coin: " nmb
+
+for ((i=1; i<=$nmb; i++))
+do
+        ran=$((RANDOM%2))
+        if [ $ran -eq 1 ]
+        then
+                ((++dict[H]))
+        else
+                ((++dict[T]))
+        fi
+done
+echo "By flipping the coin $nmb times"
+echo "it faces Head ${dict[H]} times & Tail ${dict[T]} times"
+
+win_head=$( perl -e "{print (${dict[H]}/$nmb * 100)}")
+win_tail=$( perl -e "{print (${dict[T]}/$nmb * 100)}")
