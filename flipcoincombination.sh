e6971b1825614927d70979a9cd743e324c59d511
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-05-27 01:23:58 +0530

    triplet combination

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -5,15 +5,20 @@ read -p "Enter the number to flip coin: " nmb
 function coin()
 {
 case $1 in
-        11) echo "HH" ;;
-        10) echo "HT" ;;
-        01) echo "TH" ;;
-        00) echo "TT" ;;
+        111) echo "HHH" ;;
+        110) echo "HHT" ;;
+        101) echo "HTH" ;;
+        100) echo "HTT" ;;
+        011) echo "THH" ;;
+        010) echo "THT" ;;
+        001) echo "TTH" ;;
+	000) echo "TTT" ;;
+        000) echo "TTH" ;;
 esac
 }
 for ((i=1; i<=$nmb; i++))
 do
-        ran=$((RANDOM%2))$((RANDOM%2))
+        ran=$((RANDOM%2))$((RANDOM%2))$((RANDOM%2))
         ((++dict[`coin $ran`]))
 done
 for key in ${!dict[@]}
